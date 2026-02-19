f9d8f464421086af303c50760028ff38ffb92fbe
Marta del Olmo <martadlolmo@gmail.com>
2026-02-19 17:45:27 +0100

    Paso5: eliminar el metodo mediante refactorizacion

--- src/tarea4/DelOlmoMartaED04.java
+++ src/tarea4/DelOlmoMartaED04.java
@@ -7,7 +7,7 @@ package tarea4;
 public class DelOlmoMartaED04 {
 
     public static void main(String[] args) {
-        System.out.println("Febrero de 2026 tiene " + Uitlidades.diasEnMesArray(2, 2026) + " días");
+        
         System.out.println("60mph son " + Uitlidades.kmhAms(Uitlidades.mphAKmh(60)) + " metros por segundo");
         System.out.println("Si el cateto A mide 3cm y el cateto B mide 4cm, entonces la hipotenusa mide "
                 + Uitlidades.hipotenusa(3, 4) + "cm");
